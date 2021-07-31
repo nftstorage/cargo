ae09ff951ee69dec94bfcb9bedb5c0524216b671
Peter Rabbitson <ribasushi@protocol.ai>
2021-07-31 11:14:18 +0000

    Rudimentary S3 offloader

---
+++ maint/s3_offload.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# installation:
+#
+#   sudo ln -s /srv/cargo/dagcargo/maint/s3_offload.bash  /etc/cron.hourly/car_s3_offload
+#
+
+set -e
+set -o pipefail
+
+# let someone else make this for perm reasons
+LOGDIR="/srv/cargo/LOGS/$(date -u '+%Y-%m-%d')"
+while ! [[ -d "$LOGDIR" ]]; do sleep 1; done
+
+exec 1>>"$LOGDIR/s3_offload.log"
+exec 2>&1
+
+CARGO_DATADIR="/srv/cargo/CAR_DATA"
+CARGO_NGINX_REDIRECTS_INCLUDE="/srv/cargo/car_s3_redirects"
+export CARGO_S3_BUCKET="web3-storage-carstage"
+
+[[ -z "$(ls -A "$CARGO_DATADIR" )" ]] && exit 0
+
+[[ $EUID -ne 0 ]] && echo "You must be root to run this" && exit 1
+
+aws s3 sync --no-progress --acl public-read "$CARGO_DATADIR" "s3://$CARGO_S3_BUCKET/"
+
+aws s3 ls "s3://$CARGO_S3_BUCKET/" \
+| grep -Eo '\S+\.car$' \
+| perl -ne 'chomp; ($root) = m/([^_]+)/; $full = $_; print "rewrite ^/deal-cars/$_\$  https://$ENV{CARGO_S3_BUCKET}.s3.amazonaws.com/$full permanent;\n" for $full, "$root.car"' \
+> "$CARGO_NGINX_REDIRECTS_INCLUDE"
+
+/etc/init.d/nginx reload >/dev/null
+
+aws s3 ls "s3://$CARGO_S3_BUCKET/" \
+| grep -Eo '\S+\.car$' \
+| xargs -I{} -n1 rm -f "$CARGO_DATADIR/{}"
